964c6702829716f9bcc8aa49ea29ce50f3a252f6
Parvin Farahzadeh <parv93farah@gmail.com>
2022-03-26 13:25:04 -0400

    finish functionality

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,73 @@ class App extends Component {
   state = {
     btn1 : {text: 'SELECT CIRCLE 1', btnSelected : false},
     btn2 : {text: 'SELECT CIRCLE 2', btnSelected : false},
-    btn3 : {text: 'SELECT CIRCLE 3', btnSelected : true},
+    btn3 : {text: 'SELECT CIRCLE 3', btnSelected : false},
     btn4 : {text: 'SELECT CIRCLE 4', btnSelected : false},
     crcl1 : {text: '1', selected : false},
     crcl2 : {text: '2', selected : false},
-    crcl3 : {text: '3', selected : true},
+    crcl3 : {text: '3', selected : false},
     crcl4 : {text: '4', selected : false},
+    btn1Selected: false,
+    btn2Selected: false,
+    btn3Selected: false,
+    btn4Selected: false,
+    crcl1Selected : false, 
+    crcl2Selected : false, 
+    crcl3Selected : false, 
+    crcl4Selected : false, 
+    btn1text: 'SELECT CIRCLE 1', 
+    btn2text: 'SELECT CIRCLE 2', 
+    btn3text: 'SELECT CIRCLE 3', 
+    btn4text: 'SELECT CIRCLE 4', 
 
-
-
-    selected : true,
   }
 
 
 
-  updateBtn1 () {
-    this.setState((state) => 
-    {
-      // return 
-      // {
-      //   btn1.btnSelected : !state.btn1.btnSelected, 
-      //   crcl1.selected : ! state.crcl1.selected
-      // }
+  updateBtn1 = () => {
+    this.setState((state) => {
+      // Important: read `state` instead of `this.state` when updating.
+      return {
+        btn1Selected: !state.btn1Selected.value,
+        crcl1Selected: !state.crcl1Selected.value, 
+        btn1text: 'CIRCLE 1 SELECTED'
+      }
     });
   }
 
+  updateBtn2 = () => {
+    this.setState((state) => {
+      // Important: read `state` instead of `this.state` when updating.
+      return {
+        btn2Selected: !state.btn2Selected.value,
+        crcl2Selected: !state.crcl2Selected.value,
+        btn2text: 'CIRCLE 2 SELECTED'
+      }
+    });
+  }
+
+  updateBtn3 = () => {
+    this.setState((state) => {
+      // Important: read `state` instead of `this.state` when updating.
+      return {
+        btn3Selected: !state.btn3Selected.value,
+        crcl3Selected: !state.crcl3Selected.value,
+        btn3text: 'CIRCLE 3 SELECTED'
+      }
+    });
+  }
+
+  updateBtn4 = () => {
+    this.setState((state) => {
+      // Important: read `state` instead of `this.state` when updating.
+      return {
+        btn4Selected: !state.btn4Selected.value,
+        crcl4Selected: !state.crcl4Selected.value,
+        btn4text: 'CIRCLE 4 SELECTED'
+      }
+    });
+  }
+  
 
   render() {
     return (
@@ -40,16 +82,16 @@ class App extends Component {
         <header className="App-header">UNIT 4 FINAL ASSESSMENT</header>
         <main>
           <CircleSelector 
-            text1={this.state.btn1.text} btn1Selected={this.state.btn1.btnSelected} 
-            text2={this.state.btn2.text} btn2Selected={this.state.btn2.btnSelected}
-            text3={this.state.btn3.text} btn3Selected={this.state.btn3.btnSelected}
-            text4={this.state.btn4.text} btn4Selected={this.state.btn4.btnSelected}
+            text1={this.state.btn1text} btn1Selected={this.state.btn1Selected} updateBtn1={this.updateBtn1}
+            text2={this.state.btn2text} btn2Selected={this.state.btn2Selected} updateBtn2={this.updateBtn2}
+            text3={this.state.btn3text} btn3Selected={this.state.btn3Selected} updateBtn3={this.updateBtn3}
+            text4={this.state.btn4text} btn4Selected={this.state.btn4Selected} updateBtn4={this.updateBtn4}
           />
           <Circles
-            text1={this.state.crcl1.text} crcl1Selected={this.state.crcl1.selected}
-            text2={this.state.crcl2.text} crcl2Selected={this.state.crcl2.selected}
-            text3={this.state.crcl3.text} crcl3Selected={this.state.crcl3.selected}
-            text4={this.state.crcl4.text} crcl4Selected={this.state.crcl4.selected}
+            text1={this.state.crcl1.text} crcl1Selected={this.state.crcl1Selected}
+            text2={this.state.crcl2.text} crcl2Selected={this.state.crcl2Selected}
+            text3={this.state.crcl3.text} crcl3Selected={this.state.crcl3Selected}
+            text4={this.state.crcl4.text} crcl4Selected={this.state.crcl4Selected}
           />
         </main>
       </div>
--- src/components/CircleSelector/CircleSelector.jsx
+++ src/components/CircleSelector/CircleSelector.jsx
@@ -5,9 +5,9 @@ function CircleSelector(props) {
     return (
         <div className="CircleSelector">
             <button onClick={()=>{props.updateBtn1()}} className={props.btn1Selected ? 'selected' : null }>{props.text1}</button>
-            <button onClick={()=>{props.updateBtn()}} className={props.btn2Selected ? 'selected' : null }>{props.text2}</button>
-            <button onClick={()=>{props.updateBtn()}} className={props.btn3Selected ? 'selected' : null }>{props.text3}</button>
-            <button onClick={()=>{props.updateBtn()}} className={props.btn4Selected ? 'selected' : null }>{props.text4}</button>
+            <button onClick={()=>{props.updateBtn2()}} className={props.btn2Selected ? 'selected' : null }>{props.text2}</button>
+            <button onClick={()=>{props.updateBtn3()}} className={props.btn3Selected ? 'selected' : null }>{props.text3}</button>
+            <button onClick={()=>{props.updateBtn4()}} className={props.btn4Selected ? 'selected' : null }>{props.text4}</button>
         </div>
     )
 }
